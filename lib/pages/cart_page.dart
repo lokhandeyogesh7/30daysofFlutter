b4141ff3ec85baaa7842891459ceab608fc15827
lokhandeyogesh7 <lokhandeyogesh7@gmail.com>
2021-03-18 09:00:01 +0530

    day22 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -10,6 +10,58 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
+      ),
+      body: Column(
+        children: [
+          _CartListState().p16().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$999".text.xl5.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(context.theme.buttonColor)),
+            child: "Buy".text.color(Colors.white).make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CartListState extends StatefulWidget {
+  @override
+  __CartListStateState createState() => __CartListStateState();
+}
+
+class __CartListStateState extends State<_CartListState> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(CupertinoIcons.speedometer),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
       ),
     );
   }
